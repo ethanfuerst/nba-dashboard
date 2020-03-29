31c1bfea821f23eca7052366d64b523091967fc1
Ethan Fuerst <ethanfuerst@gmail.com>
2020-03-29 12:46:02 -0500

    fixed career

--- nba_games.py
+++ nba_games.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle, Rectangle, Arc
 import datetime
 import html5lib
 from nba_api.stats.static import players
@@ -43,6 +45,16 @@ class NBA_Player:
         df = df[df['Team'] != 'TOT'][['Season', 'Team', 'TEAM_ID']].copy()
         df['start'] = df['Season'].apply(lambda x: int(x[:4]))
         df['end'] = df['start'] + 1
+        cond = df.end.sub(df.end.shift()).ne(1) | (df.Team.ne(df.Team.shift()))
+        no_year_end_change = df.end.shift(-1).sub(df.end).eq(0)
+        df['change'] = df.loc[cond,'start']
+        df['end_edit'] = np.where(no_year_end_change,df.start,df.end)
+        df['change'] = df.change.ffill().astype('Int64')
+        df = df.groupby(['Team','TEAM_ID','change']).end_edit.max().reset_index()
+        df['Years'] = df.change.astype(str).str.cat(df.end_edit.astype(str),sep='-')
+        df = df.sort_values(['change', 'end_edit'])
+        df = df.drop(['change','end_edit'],axis = 1)
+        df = df.rename({'TEAM_ID': 'Team ID'}, axis='columns')
         self.career = df
     
     def get_season(self, season=datetime.datetime.today().year - 1, season_type='regular'):
@@ -363,3 +375,74 @@ def thres_games(startyear=2000, endyear=datetime.datetime.today().year - 1, thre
         tot.append([season, count, game_nums, Projected])
     
     return pd.DataFrame(tot, columns=['Season', 'Count', 'Game Nums', 'Projected'])
+
+def draw_court(ax=None, color='black', lw=2, outer_lines=False):
+    '''
+    From http://savvastjortjoglou.com/nba-shot-sharts.html
+    '''
+    # If an axes object isn't provided to plot onto, just get current one
+    if ax is None:
+        ax = plt.gca()
+
+    # Create the various parts of an NBA basketball court
+
+    # Create the basketball hoop
+    # Diameter of a hoop is 18" so it has a radius of 9", which is a value
+    # 7.5 in our coordinate system
+    hoop = Circle((0, 0), radius=7.5, linewidth=lw, color=color, fill=False)
+
+    # Create backboard
+    backboard = Rectangle((-30, -7.5), 60, -1, linewidth=lw, color=color)
+
+    # The paint
+    # Create the outer box 0f the paint, width=16ft, height=19ft
+    outer_box = Rectangle((-80, -47.5), 160, 190, linewidth=lw, color=color,
+                          fill=False)
+    # Create the inner box of the paint, widt=12ft, height=19ft
+    inner_box = Rectangle((-60, -47.5), 120, 190, linewidth=lw, color=color,
+                          fill=False)
+
+    # Create free throw top arc
+    top_free_throw = Arc((0, 142.5), 120, 120, theta1=0, theta2=180,
+                         linewidth=lw, color=color, fill=False)
+    # Create free throw bottom arc
+    bottom_free_throw = Arc((0, 142.5), 120, 120, theta1=180, theta2=0,
+                            linewidth=lw, color=color, linestyle='dashed')
+    # Restricted Zone, it is an arc with 4ft radius from center of the hoop
+    restricted = Arc((0, 0), 80, 80, theta1=0, theta2=180, linewidth=lw,
+                     color=color)
+
+    # Three point line
+    # Create the side 3pt lines, they are 14ft long before they begin to arc
+    corner_three_a = Rectangle((-220, -47.5), 0, 140, linewidth=lw,
+                               color=color)
+    corner_three_b = Rectangle((220, -47.5), 0, 140, linewidth=lw, color=color)
+    # 3pt arc - center of arc will be the hoop, arc is 23'9" away from hoop
+    # I just played around with the theta values until they lined up with the 
+    # threes
+    three_arc = Arc((0, 0), 475, 475, theta1=22, theta2=158, linewidth=lw,
+                    color=color)
+
+    # Center Court
+    center_outer_arc = Arc((0, 422.5), 120, 120, theta1=180, theta2=0,
+                           linewidth=lw, color=color)
+    center_inner_arc = Arc((0, 422.5), 40, 40, theta1=180, theta2=0,
+                           linewidth=lw, color=color)
+
+    # List of the court elements to be plotted onto the axes
+    court_elements = [hoop, backboard, outer_box, inner_box, top_free_throw,
+                      bottom_free_throw, restricted, corner_three_a,
+                      corner_three_b, three_arc, center_outer_arc,
+                      center_inner_arc]
+
+    if outer_lines:
+        # Draw the half court line, baseline and side out bound lines
+        outer_lines = Rectangle((-250, -47.5), 500, 470, linewidth=lw,
+                                color=color, fill=False)
+        court_elements.append(outer_lines)
+
+    # Add the court elements onto the axes
+    for element in court_elements:
+        ax.add_patch(element)
+
+    return ax
